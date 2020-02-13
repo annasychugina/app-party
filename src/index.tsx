1bbcfd393febe626e6e4b5db60dcf18ffe96aefd
Анна Сычугина <a.sychugina@tinkoff.ru>
2020-02-13 18:03:33 +0300

    fix: template

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './ui/app/app';
+
+import './styles/reset.css';
+import './styles/reboot.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/ui/app/app.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './app';
+
+describe('App Component', () => {
+  const div = document.createElement('div');
+
+  it('renders without crashing', () => {
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
