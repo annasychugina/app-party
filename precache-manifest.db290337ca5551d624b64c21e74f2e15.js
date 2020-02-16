a77505b6dca021dc231073abd70f8861ffed6c3c
sychuginaanna <sychuginaanna@ya.ru>
2020-02-16 21:04:25 +0300

    Updates

---
+++ precache-manifest.db290337ca5551d624b64c21e74f2e15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1acf3f033be48d9b4cfc7e921d3e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed4f7dbb63a28ab6e873",
+    "url": "/static/css/main.463be1f1.chunk.css"
+  },
+  {
+    "revision": "075dcb08abb4ee358158",
+    "url": "/static/js/2.09cdd946.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.09cdd946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4f7dbb63a28ab6e873",
+    "url": "/static/js/main.730e2144.chunk.js"
+  },
+  {
+    "revision": "a781a45b52dd016b8cf0",
+    "url": "/static/js/runtime-main.91b7a396.js"
+  },
+  {
+    "revision": "f1d36c4bd384d843bc780cf716f92832",
+    "url": "/static/media/close.f1d36c4b.svg"
+  }
+]);
